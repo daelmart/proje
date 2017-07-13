1e9f8bb9fd066922804dd4779356f247254f46e6
daelmart <daelmart@gmail.com>
2017-07-13 16:48:55 -0300

    v 0.2
    Correções:
    - Links não repetem;
    - Mensagem de erro aparece após os links terem acabado.

--- js/controllers/projeCtrl.js
+++ js/controllers/projeCtrl.js
@@ -1,12 +1,30 @@
 angular.module("proje").controller("projeCtrl", function($scope){
 	$scope.app = "P R O J E\xa0\xa0G E N E R A T O R";
 	$scope.mensagem = "clique no botão para gerar um P R O J E";
-	$scope.urls = ['','-Tdu4uKSZ3M', 'LdpMpfp-J_I', 'Gcagz8epRN8']
+	$scope.urls = ['','-Tdu4uKSZ3M', 'LdpMpfp-J_I', 'Gcagz8epRN8'];
+	$scope.noMoreLinks = true;
+	$scope.youtube = false
+	var array= [];
 
-	$scope.geraUrl = function(){
-		var i = Math.floor((Math.random() * 3) + 1);
-		$scope.randomCode = $scope.urls[i];
-		document.getElementById("linkYT").src = "https://www.youtube.com/embed/"+$scope.randomCode;
+
+
+	$scope.geraUrl = function gimmeLinks() {
+		//var i = Math.floor((Math.random() * 3) + 1);
+		$scope.randomCode = $scope.urls[Math.floor((Math.random() * 3) + 1)];
+		if(array.indexOf($scope.randomCode)==-1){
+
+			array.push($scope.randomCode);
+			document.getElementById("linkYT").src = "https://www.youtube.com/embed/"+$scope.randomCode;
+		} else if(array.length == 3) {
+			console.log('acabou');
+			$scope.noMoreLinks = false;
+			$scope.youtube = true;
+		} else
+			gimmeLinks();
+
 	}
 
 });
+
+//achar uma forma de não repetir o processo de gerar Url
+//regExp talvez?
