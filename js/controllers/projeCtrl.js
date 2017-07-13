531b1d248c53310691b7dea21d1cb50bd14779b8
daelmart <daelmart@gmail.com>
2017-07-13 16:45:21 -0300

    v. de testes 2.0
    correções:
    . Agora não é necessário clicar mais de uma vez quando o Math.random() retorna um valor já obtido.

--- js/controllers/projeCtrl.js
+++ js/controllers/projeCtrl.js
@@ -2,25 +2,25 @@ angular.module("proje").controller("projeCtrl", function($scope){
 	$scope.app = "P R O J E\xa0\xa0G E N E R A T O R";
 	$scope.mensagem = "clique no botão para gerar um P R O J E";
 	$scope.urls = ['','-Tdu4uKSZ3M', 'LdpMpfp-J_I', 'Gcagz8epRN8'];
+	$scope.noMoreLinks = true;
+	$scope.youtube = false
 	var array= [];
 
-	$scope.geraUrl = function {
+
+
+	$scope.geraUrl = function gimmeLinks() {
 		//var i = Math.floor((Math.random() * 3) + 1);
 		$scope.randomCode = $scope.urls[Math.floor((Math.random() * 3) + 1)];
 		if(array.indexOf($scope.randomCode)==-1){
 
 			array.push($scope.randomCode);
 			document.getElementById("linkYT").src = "https://www.youtube.com/embed/"+$scope.randomCode;
-		}
-
-		else if(array.indexOf($scope.randomCode) == 1 && 2 && 3){
+		} else if(array.length == 3) {
 			console.log('acabou');
-		}
-
-		else{
-			urlRand();
-		}
-
+			$scope.noMoreLinks = false;
+			$scope.youtube = true;
+		} else
+			gimmeLinks();
 
 	}
 
